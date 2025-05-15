735323aa03512b95f0dcaf1031724082ff5bacfe
Anenqui <anenquipintadovenegas@gmail.com>
2025-05-15 16:18:51 -0600

    feat:final changes

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,10 +1,14 @@
 import Task from '../models/task.model.js'
 
 export const getTasks = async (req,res)=>{
-    const tasks = await Task.find({
+    try{
+        const tasks = await Task.find({
         user:req.user.id
     }).populate('user')
     res.json(tasks);
+    }catch(error){
+        return res.status(500).json({message:"error aqui pe"})
+    }
 };
 
 export const createTask = async (req,res)=>{
@@ -22,9 +26,13 @@ export const createTask = async (req,res)=>{
 };
 
 export const getTask = async (req,res)=>{
+    try{
     const task = await Task.findById(req.params.id).populate('user');
     if (!task) return res.status(404).json({message: 'Task not found'})
     res.json(task)
+    }catch(error){
+        return res.status(404).json({message:"Task not found"})
+    }
 };
 
 export const deleteTask = async (req,res)=>{
